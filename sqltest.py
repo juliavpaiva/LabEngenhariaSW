d63f88309b3995778d9368a264be6f8d0de0824e
Julia Paiva <43045380+juliavpaiva@users.noreply.github.com>
2020-03-13 10:15:26 -0300

    Adding sqltest.py

---
+++ sqltest.py
@@ -0,0 +1,13 @@
+import pyodbc
+server = 'terminalinformativoacademico.database.windows.net'
+database = 'terminalinformativoacademico'
+username = 'admintia'
+password = 'Senha123'
+driver= '{ODBC Driver 17 for SQL Server}'
+cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
+cursor.execute("SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName FROM [SalesLT].[ProductCategory] pc JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid")
+row = cursor.fetchone()
+while row:
+    print (str(row[0]) + " " + str(row[1]))
+    row = cursor.fetchone()
